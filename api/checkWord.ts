9b5918c3cc6c778e0cb779462e85795569e66824
abhyankar3 <abhyankar3@wisc.edu>
2026-05-04 21:13:48 -0500

    logger for correct word

--- api/checkWord.ts
+++ api/checkWord.ts
@@ -3,16 +3,18 @@ import { getOfficialWord } from "./_dailyWord.js";
 export async function POST(request: Request) {
     const body = await request.json();
     const userWord: string = body.word;
-
-    const dictRes = await fetch(
-        `https://api.dictionaryapi.dev/api/v2/entries/en/${userWord}`,
-    );
-    if (!dictRes.ok) {
-        return Response.json({ valid: false, correct: false });
-    }
-
     const officialWord = await getOfficialWord();
+    console.log("OFFICIAL WORD:", officialWord);
     const correct = userWord === officialWord.toLowerCase();
+
+    if (!correct) {
+        const dictRes = await fetch(
+            `https://api.dictionaryapi.dev/api/v2/entries/en/${userWord}`,
+        );
+        if (!dictRes.ok) {
+            return Response.json({ valid: false, correct: false });
+        }
+    }
 
     const result: number[] = []; // 2 is corrent, 1 is wrong place, 0 is wrong
 
