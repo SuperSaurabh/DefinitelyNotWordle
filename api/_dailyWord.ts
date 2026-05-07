c6ba4aa9ce7cd21a73660d2227f3e519b94334d0
abhyankar3 <abhyankar3@wisc.edu>
2026-05-07 13:44:05 -0500

    changed api to datamuse for random daily word

--- api/_dailyWord.ts
+++ api/_dailyWord.ts
@@ -2,13 +2,12 @@ let cached: { word: string; date: string } | null = null;
 
 export async function getOfficialWord(): Promise<string> {
     const today = new Date().toISOString().slice(0, 10);
-
     if (cached?.date === today) return cached.word;
 
-    const res = await fetch(
-        "https://random-word-api.herokuapp.com/word?length=5&diff=1",
-    );
-    const [word] = await res.json();
+    const res = await fetch("https://api.datamuse.com/words?sp=?????&max=1000");
+    const words = await res.json();
+    const word = words[Math.floor(Math.random() * words.length)].word;
     cached = { word, date: today };
+    console.log("OFFICIAL WORD:", word);
     return word;
 }
